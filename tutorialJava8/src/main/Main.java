70bf8078654e0e2df8261e431c9154a84bce2578
sylwesterszykula <sylwesterszykula@gmail.com>
2017-03-19 12:53:16 +0100

    Wersja testowa.

---
+++ tutorialJava8/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * Created by Sylwester Szykuła on 19.03.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Integer liczba = new Integer(10);
+        liczba = liczba + 1;
+        System.out.println(liczba);
+    }
+}
